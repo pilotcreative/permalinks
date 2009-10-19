58934b34d3f4f8e94fa14b14f081e72ab1767f83
limonka <ewa.limanowka@gmail.com>
2009-10-19 16:50:10 +0300

    Initialize commit

---
+++ lib/permalinks.rb
@@ -0,0 +1,15 @@
+module Permalinks
+  def self.included(base)
+    base.extend ClassMethods
+  end
+  
+  module ClassMethods
+    def has_permalink(field = :name)
+      define_method(:to_param) do
+        "#{id}-#{send(field).to_s.downcase.gsub(/[^[:alnum:]]/,'-')}".gsub(/-{2,}/, '-')
+      end
+    end
+  end
+end
+
+ActiveRecord::Base.send(:include, Permalinks)
