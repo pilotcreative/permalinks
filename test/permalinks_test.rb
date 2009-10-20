131aa3159e30f98ab2cb8bfbb418be64ef61999e
limonka <ewa.limanowka@gmail.com>
2009-10-20 10:33:02 +0300

    Fixed problem with white spaces in the end/front.

--- test/permalinks_test.rb
+++ test/permalinks_test.rb
@@ -9,27 +9,32 @@ Expectations do
   end
 
   expect "5-united-kingdom" do
-    p = Country.new :name => "United Kingdom"
-    p.id = 5
-    p.save!
-    p.to_param
+    c = Country.new :name => "United Kingdom"
+    c.id = 5
+    c.save!
+    c.to_param
   end
 
-  #error !!!!!!!!!!!
-  #TODO: fix it!!!!
   expect "6666-united-kingdom" do
-    p = Country.new :name => "United    Kingdom   "
-    p.id = 6666
+    c = Country.new :name => "United    Kingdom   "
+    c.id = 6666
+    c.save!
+    c.to_param
+  end
+
+  expect "11111-united-kingdom-is-cold" do
+    p = Post.new :title => "   United    Kingdom is cold   "
+    p.id = 11111
     p.save!
     p.to_param
   end
 
   #added - capital letters
   expect "6-usa" do
-    p = Country.new :name => "USA"
-    p.id = 6
-    p.save!
-    p.to_param
+    c = Country.new :name => "USA"
+    c.id = 6
+    c.save!
+    c.to_param
   end
 
   # added - should clean up strings with arbitrary spaces
