b19cd84d7ce0ef2a82b0a2216adb0df51fa2da63
Jakub Kuźma <qoobaa+github@gmail.com>
2010-02-16 09:33:10 +0100

    moved init.rb to rails/ directory

--- init.rb
+++
@@ -1,5 +0,0 @@
-require File.join(File.dirname(__FILE__), "lib", "permalinks")
-
-if defined?(ActiveRecord::Base)
-  ActiveRecord::Base.extend(Permalinks::HasPermalink)
-end
---
+++ rails/init.rb
@@ -0,0 +1,1 @@
+ActiveRecord::Base.extend(Permalinks::HasPermalink)
